f08be77ff3829f336d46c5e8539cc25461b09329
dangchien2003 <chienboy03@gmail.com>
2024-11-18 17:35:20 +0700

    init service

---
+++ orderService/src/main/java/org/example/orderservice/util/TimeUtils.java
@@ -0,0 +1,21 @@
+package org.example.orderservice.util;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class TimeUtils {
+    private TimeUtils() {
+    }
+
+    public static long getTimeStamp() {
+        return Instant.now().toEpochMilli();
+    }
+
+    public static String convertTimestampToString(long timestamp, String format) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+        LocalDateTime dateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault());
+        return dateTime.format(formatter);
+    }
+}
